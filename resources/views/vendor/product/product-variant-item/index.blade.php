9bcaf88be838c5f2b1d749fef5c1143928e068b7
Alberin Andus <130210722+suprandus@users.noreply.github.com>
2024-10-15 21:56:08 +0800

    Finish pharmacy dashboard migrations
    //User and pharmacy dashboard requires testing

--- resources/views/vendor/product/product-variant-item/index.blade.php
+++ resources/views/vendor/product/product-variant-item/index.blade.php
@@ -1,46 +1,43 @@
 @extends('vendor.layouts.master')
 
-@section('title')
+{{-- @section('title')
 {{$settings->site_name}} || Product Variant Item
-@endsection
+@endsection --}}
 
 @section('content')
-  <!--=============================
+<!--=============================
     DASHBOARD START
   ==============================-->
-  <section id="wsus__dashboard">
-    <div class="container-fluid">
-        @include('vendor.layouts.sidebar')
-
-      <div class="row">
-        <div class="col-xl-9 col-xxl-10 col-lg-9 ms-auto">
-            <a href="{{route('vendor.products-variant.index', ['product' => $product->id])}}" class="btn btn-warning mb-4"><i class="fas fa-long-arrow-left"></i> Back</a>
-          <div class="dashboard_content mt-2 mt-md-0">
-            <h3><i class="far fa-user"></i> Product Variant Item</h3>
-            <h6>Variant: {{$variant->name}}</h6>
-            <div class="create_button">
-                <a href="{{route('vendor.products-variant-item.create', ['productId' => $product->id, 'variantId' => $variant->id])}}" class="btn btn-primary"><i class="fas fa-plus"></i> Create Variant Item</a>
-            </div>
-            <div class="wsus__dashboard_profile">
-              <div class="wsus__dash_pro_area">
-                {{ $dataTable->table() }}
-              </div>
-            </div>
-          </div>
+<div class="section">
+  <div class="section-header">
+    <h1>Product Variant Item</h1>
+  </div>
+  <div class="section-body">
+    <div class="card">
+      <div class="card-header">
+        <h4>Variant: {{$variant->name}}</h4>
+        <div class="card-header-action">
+          <a href="{{route('vendor.products-variant-item.create', ['productId' => $product->id, 'variantId' => $variant->id])}}"
+            class="btn btn-primary"><i class="fas fa-plus"></i> Create Variant Item</a>
         </div>
       </div>
+      <div class="card-body">
+        {{ $dataTable->table() }}
+      </div>
     </div>
-  </section>
-  <!--=============================
+  </div>
+</div>
+</div>
+<!--=============================
     DASHBOARD START
   ==============================-->
 @endsection
 
 @push('scripts')
-    {{ $dataTable->scripts(attributes: ['type' => 'module']) }}
+{{ $dataTable->scripts(attributes: ['type' => 'module']) }}
 
-    <script>
-        $(document).ready(function(){
+<script>
+  $(document).ready(function(){
             $('body').on('click', '.change-status', function(){
                 let isChecked = $(this).is(':checked');
                 let id = $(this).data('id');
@@ -62,5 +59,5 @@
 
             })
         })
-    </script>
+</script>
 @endpush

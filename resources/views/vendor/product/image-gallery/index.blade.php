37f5ea163cf6df2bab6b7dd686172d97f2d10f8e
Alberin Andus <130210722+suprandus@users.noreply.github.com>
2024-10-15 19:30:01 +0800

    Fix image gallery for pharmacy

--- resources/views/vendor/product/image-gallery/index.blade.php
+++ resources/views/vendor/product/image-gallery/index.blade.php
@@ -1,58 +1,48 @@
 @extends('vendor.layouts.master')
 
-@section('title')
+{{-- @section('title')
 {{$settings->site_name}} || Image Gallery
-@endsection
+@endsection --}}
 
 @section('content')
-  <!--=============================
+<!--=============================
     DASHBOARD START
   ==============================-->
-  <section id="wsus__dashboard">
-    <div class="container-fluid">
-        @include('vendor.layouts.sidebar')
-
-      <div class="row">
-        <div class="col-xl-9 col-xxl-10 col-lg-9 ms-auto">
-            <a href="{{route('vendor.products.index')}}" class="btn btn-warning mb-4"><i class="fas fa-long-arrow-left"></i> Back</a>
-          <div class="dashboard_content mt-2 mt-md-0">
-            <h3><i class="fas fa-images"></i> Product: {{$product->name}}</h3>
-            <div class="wsus__dashboard_profile">
-              <div class="wsus__dash_pro_area">
-                <form action="{{route('vendor.products-image-gallery.store')}}" method="POST" enctype="multipart/form-data">
-                    @csrf
-                    <div class="form-group wsus__input">
-                        <label for="">Image <code>(Multiple image supported!)</code></label>
-                        <input type="file" name="image[]" class="form-control" multiple>
-                        <input type="hidden" name="product" value="{{$product->id}}">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Upload</button>
-                </form>
-              </div>
-            </div>
-          </div>
+<div class="section">
+  <div class="section-header">
+    <h1>Image Gallery</h1>
+  </div>
+  <div class="section-body">
+    <div class="card">
+      <div class="card-header">
+        <h4>Product Images</h4>
+        <div class="card-header-action">
+          <a href="{{route('vendor.products.index')}}" class="btn btn-warning"><i class="fas fa-arrow-left"></i> Back</a>
         </div>
       </div>
-
-      <div class="row mt-5">
-        <div class="col-xl-9 col-xxl-10 col-lg-9 ms-auto">
-          <div class="dashboard_content mt-2 mt-md-0">
-            <h3><i class="fas fa-images"></i></i> Product Images</h3>
-            <div class="wsus__dashboard_profile">
-              <div class="wsus__dash_pro_area">
-                {{ $dataTable->table() }}
-              </div>
-            </div>
+      <div class="card-body">
+        <form action="{{route('vendor.products-image-gallery.store')}}" method="POST" enctype="multipart/form-data">
+          @csrf
+          <div class="form-group">
+            <label for="">Image <code>(Multiple image supported!)</code></label>
+            <input type="file" name="image[]" class="form-control" multiple>
+            <input type="hidden" name="product" value="{{$product->id}}">
           </div>
+          <button type="submit" class="btn btn-primary">Upload</button>
+        </form>
+        <hr class="mt-4">
+        <div class="card-body">
+          {{ $dataTable->table() }}
         </div>
       </div>
     </div>
-  </section>
-  <!--=============================
+  </div>
+</div>
+<!--=============================
     DASHBOARD START
   ==============================-->
 @endsection
 
 @push('scripts')
-    {{ $dataTable->scripts(attributes: ['type' => 'module']) }}
+{{ $dataTable->scripts(attributes: ['type' => 'module']) }}
 @endpush

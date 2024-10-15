4777017aeeb2b9b59f97f6fa73e5bdf58b5c3205
Alberin Andus <130210722+suprandus@users.noreply.github.com>
2024-10-15 20:23:59 +0800

    Add initial views for product variant: index, create, edit

--- resources/views/vendor/product/product-variant/index.blade.php
+++ resources/views/vendor/product/product-variant/index.blade.php
@@ -1,46 +1,42 @@
 @extends('vendor.layouts.master')
 
-@section('title')
+{{-- @section('title')
 {{$settings->site_name}} || Product Variant
-@endsection
+@endsection --}}
 
 @section('content')
-  <!--=============================
+<!--=============================
     DASHBOARD START
   ==============================-->
-  <section id="wsus__dashboard">
-    <div class="container-fluid">
-        @include('vendor.layouts.sidebar')
-
-      <div class="row">
-        <div class="col-xl-9 col-xxl-10 col-lg-9 ms-auto">
-            <a href="{{route('vendor.products.index')}}" class="btn btn-warning mb-4"><i class="fas fa-long-arrow-left"></i> Back</a>
-          <div class="dashboard_content mt-2 mt-md-0">
-            <h3><i class="far fa-user"></i> Product Variant</h3>
-            <h6>Product: {{$product->name}}</h6>
-            <div class="create_button">
-                <a href="{{route('vendor.products-variant.create', ['product' => $product->id])}}" class="btn btn-primary"><i class="fas fa-plus"></i> Create Variant</a>
-            </div>
-            <div class="wsus__dashboard_profile">
-              <div class="wsus__dash_pro_area">
-                {{ $dataTable->table() }}
-              </div>
-            </div>
-          </div>
+<div class="section">
+  <div class="section-header">
+    <h1>Product Variant</h1>
+  </div>
+  <div class="section-body">
+    <div class="card">
+      <div class="card-header">
+        <h4>Product: {{$product->name}}</h4>
+        <div class="card-header-action">
+          <a href="{{route('vendor.products-variant.create', ['product' => $product->id])}}" class="btn btn-primary"><i
+              class="fas fa-plus"></i> Create Variant</a>
         </div>
       </div>
+      <div class="card-body">
+        {{ $dataTable->table() }}
+      </div>
     </div>
-  </section>
-  <!--=============================
+  </div>
+</div>
+<!--=============================
     DASHBOARD START
   ==============================-->
 @endsection
 
 @push('scripts')
-    {{ $dataTable->scripts(attributes: ['type' => 'module']) }}
+{{ $dataTable->scripts(attributes: ['type' => 'module']) }}
 
-    <script>
-        $(document).ready(function(){
+<script>
+  $(document).ready(function(){
             $('body').on('click', '.change-status', function(){
                 let isChecked = $(this).is(':checked');
                 let id = $(this).data('id');
@@ -62,5 +58,5 @@
 
             })
         })
-    </script>
+</script>
 @endpush

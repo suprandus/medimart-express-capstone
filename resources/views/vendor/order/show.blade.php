74e65534c2c6d896ee3746ee9ca9265532806023
Alberin Andus <130210722+suprandus@users.noreply.github.com>
2024-11-07 16:24:48 +0800

    fix: correct spelling of sidebar and show label

--- resources/views/vendor/order/show.blade.php
+++ resources/views/vendor/order/show.blade.php
@@ -74,10 +74,10 @@ $address = json_decode($order->order_address);
                                     <th data-width="40">#</th>
                                     <th>Item</th>
                                     <th>Variant</th>
-                                    <th>Vendor Name</th>
+                                    <th>Pharmacy Name</th>
                                     <th class="text-center">Price</th>
                                     <th class="text-center">Quantity</th>
-                                    <th class="text-right">Totals</th>
+                                    <th class="text-right">Total</th>
                                 </tr>
                                 @foreach ($order->orderProducts as $product)
                                 @php

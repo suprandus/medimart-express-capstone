e6e1f82f8ad7a7f1a5370c4e6e9661d8fa5b094c
Alberin Andus <130210722+suprandus@users.noreply.github.com>
2024-11-17 21:16:09 +0800

    fix: change the word from `seller` to `pharmacy`

--- resources/views/admin/product/seller-pending-product/index.blade.php
+++ resources/views/admin/product/seller-pending-product/index.blade.php
@@ -13,7 +13,7 @@
               <div class="col-12">
                 <div class="card">
                   <div class="card-header">
-                    <h4>All Seller Pending Products</h4>
+                    <h4>All Pharmacy Pending Products</h4>
                   </div>
                   <div class="card-body">
                     {{ $dataTable->table() }}
--- resources/views/admin/product/seller-product/index.blade.php
+++ resources/views/admin/product/seller-product/index.blade.php
@@ -13,7 +13,7 @@
               <div class="col-12">
                 <div class="card">
                   <div class="card-header">
-                    <h4>All Seller Products</h4>
+                    <h4>All Pharmacy Products</h4>
                   </div>
                   <div class="card-body">
                     {{ $dataTable->table() }}

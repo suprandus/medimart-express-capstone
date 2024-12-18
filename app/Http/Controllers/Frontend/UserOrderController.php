ce5c78bb00f49c0ff00b350f78b997e15b4dc627
Alberin Andus <130210722+suprandus@users.noreply.github.com>
2024-12-18 19:19:36 +0800

    fix: add quantity back to products
    - If the order is cancelled it will automatically to qty
    - No further validation
    - This is only on USER side

--- app/Http/Controllers/Frontend/UserOrderController.php
+++ app/Http/Controllers/Frontend/UserOrderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Frontend;
 use App\DataTables\UserOrderDataTable;
 use App\Http\Controllers\Controller;
 use App\Models\Order;
+use App\Models\Product;
 use App\Models\PaymongoSetting;
 use Illuminate\Http\Request;
 use Luigel\Paymongo\Facades\Paymongo;
@@ -59,6 +60,13 @@ class UserOrderController extends Controller
             $order->order_status = $request->status;
             $order->payment_status = 'cancelled';
 
+            // Add quantity back to products
+            foreach ($order->orderProducts as $orderProduct) {
+                $product = Product::find($orderProduct->product_id);
+                $product->qty += $orderProduct->qty;
+                $product->save();
+            }
+
             $order->save();
             toastr('Updated order status', 'success', 'Success');
         } else if ($paymentMethod === 'paymongo') {
@@ -76,6 +84,13 @@ class UserOrderController extends Controller
 
             $order->order_status = $request->status;
             $order->payment_status = 'refunded';
+
+            // Add quantity back to products
+            foreach ($order->orderProducts as $orderProduct) {
+                $product = Product::find($orderProduct->product_id);
+                $product->qty += $orderProduct->qty;
+                $product->save();
+            }
 
             $order->save();
             toastr('Updated order status', 'success', 'Success');

c4c56efdbc3e50bc8d55b7b8c3adc412d4c92260
Alberin Andus <130210722+suprandus@users.noreply.github.com>
2024-12-20 20:30:00 +0800

    fix: resolve typo

--- resources/views/frontend/pages/product.blade.php
+++ resources/views/frontend/pages/product.blade.php
@@ -16,7 +16,7 @@
                         <h4>products</h4>
                         <ul>
                             <li><a href="#">home</a></li>
-                            <li><a href="#">peoduct</a></li>
+                            <li><a href="#">product</a></li>
                         </ul>
                     </div>
                 </div>

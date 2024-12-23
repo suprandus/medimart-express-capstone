fa75089d3564329bd3906932b5b7b2b72eb47bdc
Alberin Andus <130210722+suprandus@users.noreply.github.com>
2024-12-23 22:18:39 +0800

    refactor: form item amount to two decimal places [FRONTEND]

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -38,11 +38,11 @@
             <a class="wsus__pro_name" href="{{route('product-detail', $product->slug)}}">{{limitText($product->name,
                 52)}}</a>
             @if(checkDiscount($product))
-            <p class="wsus__price">{{$settings->currency_icon}}{{$product->offer_price}}
-                <del>{{$settings->currency_icon}}{{$product->price}}</del>
+            <p class="wsus__price">{{$settings->currency_icon}}{{number_format($product->offer_price, 2)}}
+                <del>{{$settings->currency_icon}}{{number_format($product->price, 2)}}</del>
             </p>
             @else
-            <p class="wsus__price">{{$settings->currency_icon}}{{$product->price}}</p>
+            <p class="wsus__price">{{$settings->currency_icon}}{{number_format($product->price, 2)}}</p>
             @endif
             <form class="shopping-cart-form">
                 <input type="hidden" name="product_id" value="{{$product->id}}">
@@ -52,7 +52,7 @@
                     @foreach ($variant->productVariantItems as $variantItem)
                     @if ($variantItem->status != 0)
                     <option value="{{$variantItem->id}}" {{$variantItem->is_default == 1 ? 'selected' :
-                        ''}}>{{$variantItem->name}} (${{$variantItem->price}})</option>
+                        ''}}>{{$variantItem->name}} (${{number_format($variantItem->price, 2)}})</option>
                     @endif
                     @endforeach
                 </select>

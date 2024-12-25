761c0df65163542dffcfb0cc1680a7963877ba92
Alberin Andus <130210722+suprandus@users.noreply.github.com>
2024-12-25 20:11:39 +0800

    fix: remove column for warning [PHARMACY]
    - The warning is for low on stock products
    - Replace it with navbar notifications

--- app/DataTables/VendorProductDataTable.php
+++ app/DataTables/VendorProductDataTable.php
@@ -21,12 +21,6 @@ class VendorProductDataTable extends DataTable
     public function dataTable(QueryBuilder $query): EloquentDataTable
     {
         return (new EloquentDataTable($query))
-            // Add the warning icon if quantity is less than 10
-            ->addColumn('warning', function ($query) {
-                $quantity = $query->qty;
-                // Add the warning icon if quantity is less than 10
-                return $quantity < 10 ? '<i class="fas fa-exclamation-circle text-danger warning-icon" title="Low stock"></i>' : '';
-            })
             ->addColumn('action', function ($query) {
                 $editBtn = "<a href='" . route('vendor.products.edit', $query->id) . "' class='btn btn-primary'><i class='far fa-edit'></i></a>";
 
@@ -139,7 +133,6 @@ class VendorProductDataTable extends DataTable
     public function getColumns(): array
     {
         return [
-            Column::computed('warning')->width(50)->addClass('text-center')->title('Warning'),
             // Column::make('id'),
             Column::make('image')->width(150),
             Column::make('name'),
